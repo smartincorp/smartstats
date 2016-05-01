a252d8822f748f85c5341e2c2a119606a4135b5e
Karan Sanjeev <smartclashcoc@gmail.com>
2016-05-01 15:53:41 +0530

    Added a new processor

--- config/config.php
+++ config/config.php
@@ -1,29 +1,30 @@
 <?php
 $conf = (object) array(
 	"meta" => (object) array(
-		"server_ip" => "52.37.128.5", // Game Server' IP
-		"server_port" => "9339",// Port to check for the server on
-		"debug_port" => "9332", //Debug port to fetch data from API (Will be used Afterwards)
-		"Api_Key" => "ucs", //key for the API (Will be used afterwards)
 		"server_name" => "Smart Clash - Status",
 		"discription" => "Smart Clash is a best in class private server for clash of clans ever made. Now ranks worlds top 7 as of coc servers.com. Please vote us at coc-servers.com",
 	)
 	);
 ?>
-	
-<?php if ($isOnline = @fsockopen($conf->meta->server_ip,$conf->meta->server_port,$errno,$errstr)) {
-	  fclose($isOnline);//Establish connection to api if server online
+<?php //Edit this as of your liking!
+          $ip = "52.37.128.5";
+          $debug = "9332";
+          $port = "9339";
+          $key = "ucs";
+?>
+<?php if ($isOnline = @fsockopen($ip,$port,$errno,$errstr)) {
+	  fclose($isOnline);  //Establish connection to api if server online
 				
-		$serverStatus = '<div class="callout callout-success"><h4>Server Online!</h4><p>Server is online, you can start playing with us!</div>';
-        $memClans = file_get_contents('http://52.37.128.5:9332/ucs/inmemclans');
-        $onPlayers = file_get_contents('http://52.37.128.5:9332/ucs/onlineplayers');
-        $players = file_get_contents('http://52.37.128.5:9332/ucs/totalclients');
-        $usedram = file_get_contents('http://52.37.128.5:9332/ucs/ram');
+	$serverStatus = '<div class="callout callout-success"><h4>Server Online!</h4><p>Server is online, you can start playing with us!</div>';
+        $memClans = file_get_contents("http://$ip:$debug/$key/inmemclans");
+        $onPlayers = file_get_contents("http://$ip:$debug/$key/onlineplayers");
+        $players = file_get_contents("http://$ip:$debug/$key/totalclients");
+        $usedram = file_get_contents("http://$ip:$debug/$key/ram");
         
-  } else {//Else display N/A instead if throwing 404 and reuining the page!
+  } else {  //Else display N/A instead if throwing 404 and reuining the page!
 			
-		$serverStatus = '<div class="callout callout-danger"><h4>Server Offline!</h4><p>Server is Offline, we are working on to fix it! Will start to work soon.</p></div>';
-		$memClans = "N/A";
+	    $serverStatus = '<div class="callout callout-danger"><h4>Server Offline!</h4><p>Server is Offline, we are working on to fix it! Will start to work soon.</p></div>';
+	    $memClans = "N/A";
 	    $onPlayers = "N/A";
 	    $players = "N/A";
 	    $usedram = "N/A";
