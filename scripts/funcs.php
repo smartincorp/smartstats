44b42845d0402787e88ea670184b6cac5a3af723
Alpha <smartclashcoc@gmail.com>
2016-05-03 22:52:47 +0530

    Update funcs.php

--- scripts/funcs.php
+++ scripts/funcs.php
@@ -1,12 +1,12 @@
 <?php
     
-    /* API installation
+        /* API installation
 	–––––––––––––––––––––––––––––––––––––––––––––––––– */
 	
 	 if ($isOnline = @fsockopen($server_Ip,$server_Port,$errno,$errstr)) {
 	  fclose($isOnline);  //Establish connection to api if server online
 				
-		$serverStatus = '<div class="callout callout-success"><h4>Server Online!</h4><p>Server is online, you can start playing with us!</div>';
+        $serverStatus = '<div class="callout callout-success"><h4>Server Online!</h4><p>Server is online, you can start playing with us!</div>';
         $memClans = file_get_contents("http://$server_Ip:$server_Debug/$server_key/inmemclans");
         $onPlayers = file_get_contents("http://$server_Ip:$server_Debug/$server_key/onlineplayers");
         $players = file_get_contents("http://$server_Ip:$server_Debug/$server_key/totalclients");
@@ -16,8 +16,8 @@
         
     } else {  //Else display N/A instead if throwing 404 and reuining the page!
 			
-		$serverStatus = '<div class="callout callout-danger"><h4>Server Offline!</h4><p>Server is Offline, we are working on to fix it! Will start to work soon.</p></div>';
-		$memClans = "N/A";
+	    $serverStatus = '<div class="callout callout-danger"><h4>Server Offline!</h4><p>Server is Offline, we are working on to fix it! Will start to work soon.</p></div>';
+	    $memClans = "N/A";
 	    $onPlayers = "N/A";
 	    $players = "N/A";
 	    $usedram = "N/A";
@@ -25,18 +25,20 @@
 	    
 	}
 	
-    /* Version Checker
+        /* Version Checker
 	–––––––––––––––––––––––––––––––––––––––––––––––––– */
-	
-	 $update = file_get_contents('https://static.smartclashcoc.com/smartstats/update.txt'); 
+	 $Version = "2.0"; //Prestent script version
+	 $update = file_get_contents('https://static.smartclashcoc.com/smartstats/update.txt'); //Url for global version
+	 $update_Url = file_get_contents('https://static.smartclashcoc.com/smartstats/updateurl.txt'); //Url for update
 	 
-    /* Update notification
+        /* Update notification
 	–––––––––––––––––––––––––––––––––––––––––––––––––– */
 	
     if($update > $Version) {
-        $update_Avail = "<div class=\"callout callout-warning\"><h4>Update Available!</h4><p>Smart Stats has an update. Download it from <a href=\"https://github.com/smartclash/statuspage/releases\" target=\"_blank\">Here</a></div>";
-    } else {
-		$update_Avail = "";
-	}
+        $update_Avail = "<div class=\"callout callout-warning\"><h4>Update Available!</h4><p>Smart Stats has an update. Download it from <a href=\"$update_Url\" target=\"_blank\">Here</a></div>";
+    }
+    else {
+    	$update_Avail = "&nbsp"; //Prevent error
+    }
     
 ?>
